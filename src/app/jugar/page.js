92cc210d7211506a2e01e8901450ff63a5266e89
TomasFZ <tomyfernandezabala@gmail.com>
2024-08-17 22:18:23 -0300

    skfns

--- src/app/jugar/page.js
+++ src/app/jugar/page.js
@@ -5,6 +5,8 @@ import { useEffect, useState } from 'react';
 export default function Jugar() {
   const [paises, setPaises] = useState([]);
   const [paisRandom, setPaisRandom] = useState([])
+  const [ronda, setRonda] = useState(1);
+  const [respuesta, setRespuesta] = useState("")
   useEffect(() => {
     fetch('https://countriesnow.space/api/v0.1/countries/flag/images')
       .then(response => response.json())
@@ -19,21 +21,28 @@ export default function Jugar() {
       mostrarPais();
     }
   }, [paises]);
-  var ronda = 0 
+
+ 
+  useEffect(() => {
+    if(paisRandom.name === respuesta)
+      setRonda(ronda+1)
+  }, []);
 
   function mostrarPais(){
     const randomIndex = Math.floor(Math.random() * paises.length);
     setPaisRandom(paises[randomIndex])
   }
 
+  function handleInputRespuesta(e){
+    setRespuesta(e.target.value)
+  }
+
   return (
     <div>
       <h2>¿Cuál es este país?</h2>
-        
           <img src={paisRandom.flag} alt="fotoPais" />
-          <input type="text" id={`nombre-${paisRandom.name}`} name="nombre" />
-        
-      
+          <input type="text" id={`nombre-${paisRandom.name}`} value={respuesta} name="nombre" onChange={handleInputRespuesta} />
+          <p>ronda: {ronda}</p>
     </div>
   );
 }
