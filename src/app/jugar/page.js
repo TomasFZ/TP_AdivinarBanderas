0f888c525315f759d090750ade35c2e49357930d
Temporaly <129062162+Temporaly@users.noreply.github.com>
2024-08-20 18:32:14 -0300

    Hola, hice el login! Quería que guarde records pero el juego no termina LMAO

--- src/app/jugar/page.js
+++ src/app/jugar/page.js
@@ -1,12 +1,30 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function Jugar() {
   const [paises, setPaises] = useState([]);
-  const [paisRandom, setPaisRandom] = useState([])
+  const [paisRandom, setPaisRandom] = useState([]);
   const [ronda, setRonda] = useState(1);
-  const [respuesta, setRespuesta] = useState("")
+  const [respuesta, setRespuesta] = useState("");
+  const [isMounted, setIsMounted] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  useEffect(() => {
+    if (isMounted) {
+      const savedUser = localStorage.getItem('user');
+      if (!savedUser) {
+        alert('Debes iniciar sesión para jugar.');
+        router.push('/');
+      }
+    }
+  }, [isMounted, router]);
+
   useEffect(() => {
     fetch('https://countriesnow.space/api/v0.1/countries/flag/images')
       .then(response => response.json())
@@ -22,27 +40,39 @@ export default function Jugar() {
     }
   }, [paises]);
 
- 
   useEffect(() => {
-    if(paisRandom.name === respuesta)
-      setRonda(ronda+1)
-  }, []);
+    if (paisRandom.name === respuesta) {
+      setRonda(ronda + 1);
+      setRespuesta("");
+      mostrarPais();
+    }
+  }, [respuesta]);
 
-  function mostrarPais(){
+  function mostrarPais() {
     const randomIndex = Math.floor(Math.random() * paises.length);
-    setPaisRandom(paises[randomIndex])
+    setPaisRandom(paises[randomIndex]);
   }
 
-  function handleInputRespuesta(e){
-    setRespuesta(e.target.value)
+  function handleInputRespuesta(e) {
+    setRespuesta(e.target.value);
   }
 
   return (
     <div>
       <h2>¿Cuál es este país?</h2>
+      {paisRandom.flag && (
+        <>
           <img src={paisRandom.flag} alt="fotoPais" />
-          <input type="text" id={`nombre-${paisRandom.name}`} value={respuesta} name="nombre" onChange={handleInputRespuesta} />
-          <p>ronda: {ronda}</p>
+          <input
+            type="text"
+            id={`nombre-${paisRandom.name}`}
+            value={respuesta}
+            name="nombre"
+            onChange={handleInputRespuesta}
+          />
+          <p>Ronda: {ronda}</p>
+        </>
+      )}
     </div>
   );
 }
